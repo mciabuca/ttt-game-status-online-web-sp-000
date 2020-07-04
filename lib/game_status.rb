85acc99f1b932fdb480d024dc7d1d558a125fc9d
Michael Ciabuca <ciabuca@gmail.com>
2020-07-04 00:41:31 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,4 +8,13 @@ end
 
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+    [0,4,8])
+      expect(WIN_COMBINATIONS).to include_array([6,4,2])
+  ]
