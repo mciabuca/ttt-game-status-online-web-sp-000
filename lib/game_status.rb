b97763fdd421dd06297a089ad68ebcd1096ddacc
Michael Ciabuca <ciabuca@gmail.com>
2020-07-04 01:31:31 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,19 +4,19 @@ def position_taken?(board, index)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+  WIN_COMBINATIONS.each {|win_combination|
+    index_0 = win_combination[0]
+    index_1 = win_combination[1]
+    index_2 = win_combination[2]
 
     position_1 = board[index_0]
     position_2 = board[index_1]
     position_3 = board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
+      return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+      return win_combination
     end
   }
   return false
