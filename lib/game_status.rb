636abe25f00d5ff2ce620df2e8b647ecd541408f
Michael Ciabuca <ciabuca@gmail.com>
2020-07-04 01:23:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ def won?(board)
     
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination
-      elsif position_1 == 'O' && position_2 == 'O' && position_3 == 'O'
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         false
       end
   end
